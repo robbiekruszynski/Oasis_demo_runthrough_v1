f519d53bb0e74fcbc2211297f09a08bca87cf6ff
robbiekruszynski <robbiekruszynski@gmail..com>
2024-06-20 11:05:33 +0200

    round one of hh.config

---
+++ harhat.config.ts
@@ -0,0 +1,48 @@
+import "@oasisprotocol/sapphire-hardhat";
+import { HardhatUserConfig} from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+
+const TEST HDWALLET = {
+    mnemonic: "test",
+    paath: "m/44'/60'/0'/0",
+    initalIndex: 0,
+    count: 20,
+    passphrase: "",
+};
+
+const accounts = process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : TEST_HDWALLET;
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: '0.8.19',
+        settings: {
+            optimizer: {
+                enabled: true
+                 
+            }
+        }
+    }, 
+    networks: {
+    hardhat: {
+        chainId: 1337,
+    },
+    'sapphire' : {
+        url:'https://sapphire.oasis.io',
+        chainId: 0x5afe,
+        accounts,
+    },
+    'sapphire-testnet': {
+        url: 'https://testnet.sapphire.oasis.dev',
+        chainId: 0x5aff,
+        accounts,
+    },
+    'sapphire-localnet': {
+        url: 'http://localhost:8545',
+        chainId: 0x5afd,
+        accounts,
+    },
+    }
+    };
+
+export default config;
